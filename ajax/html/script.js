84fdeaf39c6e3dbafa9d703d83013a30a2df316f
nagayama <nagayama@iroirodesign.com>
2015-05-16 17:23:47 +0900

    change base files

---
+++ ajax/html/script.js
@@ -0,0 +1,25 @@
+(function($) {
+
+  /*//
+  var p = $("#content");
+
+  $.ajax("/data/data.txt").done(function(data){
+    console.log(data);
+  }).fail(function(e,msg) {
+    console.log(e,msg);
+  });
+
+  $.ajax("/data/data.json").done(function(data){
+    console.log(data);
+  }).fail(function(e,msg) {
+    console.log(e,msg);
+  });
+
+  $.ajax("http://ngym.org/iroiroschool/data.js", {dataType: "jsonp"});
+
+  window.updateContent = function(data) {
+    console.log(data);
+  }
+  //*/
+
+})(jQuery);
